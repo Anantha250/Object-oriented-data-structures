a44fa42a9fba24f2be7ea25f2c694656d226c5a1
Anantha250 <129850658+Anantha250@users.noreply.github.com>
2025-02-05 21:38:40 +0700

    commit

--- Sort/Sort1.py
+++ Sort/Sort1.py
@@ -1,14 +1,14 @@
 def bubblesortRecursive(list):
-    cnt = 0
+    cnp = 0
     for i in range(len(list)):
         for j in range(len(list)-1-i):
            cnt+= 1
            if list[j] < list[j+1]:
                list[j],list[j+1] = list[j+1],list[j]
     
-    print(cnt)
+    print(cnp)
 
 inp = list(map(int,input("Enter Input :").split()))
 bubblesortRecursive(inp)
-print(inp) 
+print(inp)
     
